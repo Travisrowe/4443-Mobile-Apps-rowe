fa53aee0396c72200e9c72c113bfb2d420a8cc22
Travisrowe <travisrowe18@gmail.com>
2018-10-16 18:33:53 -0500

    hw

--- assignments/travis-location/src/app/app.module.ts
+++ assignments/travis-location/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Geolocation } from '@ionic-native/geolocation'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -19,11 +21,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- assignments/travis-location/src/pages/home/home.ts
+++ assignments/travis-location/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation'
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the HomePage page.
@@ -23,7 +25,7 @@ export class HomePage
               //the one we added #map to in home.html
   map: any; //member variable to hold our map
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) 
   {
   }
 
@@ -34,16 +36,52 @@ export class HomePage
 
   loadMap()
   {
-    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
 
-    let mapOptions = 
-    {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      let mapOptions = 
+      {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    }, (err) => {
+      console.log(err);
+    })
+
+    
+  }
+
+  addMarker()
+  {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content)
+  {
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    })
+  }
+
+  goToLoginPage()
+  {
+    this.navCtrl.push(LoginPage);
   }
 
 }
